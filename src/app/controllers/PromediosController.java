8070b0613bafcdaaa476fa765e19545fec88ea95
davq23 <davidquinterogranadillo@gmail.com>
2021-04-13 10:41:12 -0400

    Cálculo de promedios

---
+++ src/app/controllers/PromediosController.java
@@ -0,0 +1,39 @@
+package app.controllers;
+
+
+public class PromediosController {
+    public static double[] getPromediosPorEstudiante(double[][] calificaciones) {
+        // Calificaciones por estudiante (por fila)
+        double[] promedios = new double[calificaciones.length];
+        
+        for (int i = 0; i < calificaciones.length; i++) {
+            // Sumatoria de notas
+            for (int j = 0; j < calificaciones[i].length; j++) {
+                promedios[i] += calificaciones[i][j];
+            }
+
+            // División por número de notas
+            promedios[i] /= calificaciones[i].length;
+        }
+
+        return promedios;
+    }
+
+    public static double[] getPromediosPorMateria(double[][] calificaciones) {
+        // Calificaciones por materia (por columna)
+        double[] promedios = new double[calificaciones.length];
+    
+        // Asumiendo igual número de columnas por fila
+        for (int i = 0; i < calificaciones[0].length; i++) {
+            // Sumatoria de notas
+            for (int j = 0; j < calificaciones.length; j++) {
+                promedios[i] += calificaciones[j][i];
+            }
+
+            // División por número de notas
+            promedios[i] /= calificaciones[0].length;
+        }
+
+        return promedios;
+    }
+}
